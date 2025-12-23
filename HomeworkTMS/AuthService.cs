04f2d548f50de8e90b97fe999211fa1c12ea5931
echpochmak770 <echpochmak770@gmail.com>
2025-12-23 18:48:25 +0300

    exceptions-update

--- HomeworkTMS/AuthService.cs
+++ HomeworkTMS/AuthService.cs
@@ -1,56 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace HomeworkTMS
 {
     internal static class AuthService
     {
+        private static readonly Action<string>[] LoginRules =
+        {
+            value =>
+            {
+                if (value is null)
+                    throw new ArgumentNullException(nameof(value), "Логин не может быть null");
+            },
+            value =>
+            {
+                if (value.Length > 20)
+                    throw new WrongLoginException(
+                        "Длина логина не может превышать 20",
+                        LoginFailureReason.LengthBiggerThan20);
+            },
+            value =>
+            {
+                if (value.Any(char.IsWhiteSpace))
+                    throw new WrongLoginException(
+                        "Логин не может содержать пробелы",
+                        LoginFailureReason.HasWhitespaces);
+            }
+        };
+
+        private static readonly Action<string>[] PasswordRules =
+        {
+            value =>
+            {
+                if (value is null)
+                    throw new ArgumentNullException(nameof(value), "Пароль не может быть null");
+            },
+            value =>
+            {
+                if (value.Length > 20)
+                    throw new WrongPasswordException(
+                        "Длина пароля не может превышать 20",
+                        PasswordFailureReason.LengthBiggerThan20);
+            },
+            value =>
+            {
+                if (value.Any(char.IsWhiteSpace))
+                    throw new WrongPasswordException(
+                        "Пароль не может содержать пробелы",
+                        PasswordFailureReason.HasWhiteSpaces);
+            },
+            value =>
+            {
+                if (!value.Any(char.IsDigit))
+                    throw new WrongPasswordException(
+                        "В пароле должна быть хотя бы одна цифра",
+                        PasswordFailureReason.HasNoDigits);
+            }
+        };
+
         public static bool CheckCredentials(string login, string password, string confirmPassword)
         {
-            if (login is null)
+            foreach (var rule in LoginRules)
             {
-                throw new ArgumentNullException("Логин не может быть null");
+                rule(login);
             }
 
-            if (password is null)
+            foreach (var rule in PasswordRules)
             {
-                throw new ArgumentNullException("Пароль не может быть null");
+                rule(password);
             }
 
             if (confirmPassword is null)
             {
-                throw new ArgumentNullException("Подтверждение пароля не может быть null");
-            }
-
-            if (login.Length > 20)
-            {
-                throw new WrongLoginException("Длина логина не может превышать 20", LoginFailureReason.LengthBiggerThan20);
-            }
-
-            if (login.Any(char.IsWhiteSpace))
-            {
-                throw new WrongLoginException("Логин не может содержать пробелы", LoginFailureReason.HasWhitespaces);
-            }
-
-            if (password.Length > 20)
-            {
-                throw new WrongPasswordException("Длина пароля не может превышать 20", PasswordFailureReason.LengthBiggerThan20);
-            }
-
-            if (password.Any(char.IsWhiteSpace))
-            {
-                throw new WrongPasswordException("Пароль не может одержать пробелы", PasswordFailureReason.HasWhiteSpaces);
-            }
-
-            if (!password.Any(char.IsDigit))
-            {
-                throw new WrongPasswordException("В пароле должна быть хотя бы одна цифра", PasswordFailureReason.HasNoDigits);
+                throw new ArgumentNullException(nameof(confirmPassword),
+                    "Подтверждение пароля не может быть null");
             }
 
             if (!password.Equals(confirmPassword))
             {
-                throw new WrongPasswordException("Пароли не совпадают", PasswordFailureReason.ConfirmPasswordMismatch);
+                throw new WrongPasswordException(
+                    "Пароли не совпадают",
+                    PasswordFailureReason.ConfirmPasswordMismatch);
             }
 
             return true;
