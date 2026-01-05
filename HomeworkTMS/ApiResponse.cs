98d013ea6405f77bb743f1d05e4dcff05157fa73
echpochmak770 <echpochmak770@gmail.com>
2026-01-05 20:18:34 +0300

    Network homework

---
+++ HomeworkTMS/ApiResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HomeworkTMS
+{
+    internal class ApiResponse
+    {
+        private string id;
+        private string url;
+        private int width;
+        private int height;
+
+        [JsonPropertyName("id")]
+        public string Id { get => id; set { id = value; } }
+
+        [JsonPropertyName("url")]
+        public string Url { get => url; set { url = value; } }
+
+        [JsonPropertyName("width")]
+        public int Width { get => width; set { width = value; } }
+
+        [JsonPropertyName("height")]
+        public int Height { get => height; set { height = value; } }
+
+        public ApiResponse(string id, string url, int width, int height)
+        {
+            Id = id;
+            Url = url;
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ HomeworkTMS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace HomeworkTMS
+{
+    internal class Program
+    {
+        static readonly string _catApiEndpoint = "https://api.thecatapi.com/v1/images/search";
+
+        static async Task Main(string[] args)
+        {
+            using HttpClient _client = new();
+
+            string response = await _client.GetStringAsync(_catApiEndpoint);
+            var catData = JsonSerializer.Deserialize<List<ApiResponse>>(response);
+
+            if (catData.Count > 0)
+            {
+                Console.WriteLine($"Успех! Котики получены" +
+                    $"\n{catData[0].Url}");
+            }
+            else
+            {
+                Console.WriteLine("Сегодня без котиков. Что-то поломалось((");
+            }
+                
+        }
+    }
+}
