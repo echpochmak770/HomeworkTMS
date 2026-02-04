6fbebdd1cc70b8574725a5f85988007bbb9beda2
echpochmak770 <echpochmak770@gmail.com>
2026-02-04 17:34:04 +0300

    db-first sports league

---
+++ EFCore_Db-First/Models/Coach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore_Db_First.Models;
+
+public partial class Coach
+{
+    public int CoachId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public virtual ICollection<Team> Teams { get; set; } = new List<Team>();
+}
---
+++ EFCore_Db-First/Models/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore_Db_First.Models;
+
+public partial class Schedule
+{
+    public int ScheduleId { get; set; }
+
+    public int GroupId { get; set; }
+
+    public int TeamAid { get; set; }
+
+    public int TeamBid { get; set; }
+
+    public DateTime MatchTime { get; set; }
+
+    public virtual Group Group { get; set; } = null!;
+
+    public virtual Team TeamA { get; set; } = null!;
+
+    public virtual Team TeamB { get; set; } = null!;
+}
