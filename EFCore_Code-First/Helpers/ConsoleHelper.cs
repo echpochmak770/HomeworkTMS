d1a333beb88163ce50b1bf419496d4a094ac3bd6
echpochmak770 <echpochmak770@gmail.com>
2026-02-07 14:36:13 +0300

    LINQ-to-Database

---
+++ EFCore_Code-First/Helpers/ConsoleHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore_Code_First.Helpers
+{
+    public static class ConsoleHelper
+    {
+        public static void PrintResult(string title, object data)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\n--- {title.ToUpper()} ---");
+            Console.ResetColor();
+
+            if (data is IEnumerable<object> list)
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine(data?.ToString() ?? "Данные не найдены");
+            }
+            Console.WriteLine(new string('-', title.Length + 8));
+        }
+    }
+}
