70f6c184ce9f36dfd9ddc220b9234e8115105eab
echpochmak770 <echpochmak770@gmail.com>
2026-02-10 20:49:29 +0300

    init

---
+++ EFCorePractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFCorePractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
